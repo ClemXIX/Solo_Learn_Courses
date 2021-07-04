849e1a1767c85b2e0224e9f55403aa39ceaa8e65
ClemXiX <clemsbo51@gmail.com>
2021-07-04 15:15:53 +0200

    Hello GitHub

---
+++ C#/04.2_Your_First_Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cours_SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 2;
+
+            Console.Write(a);
+            Console.Write(b);
+        }
+    }
+}
