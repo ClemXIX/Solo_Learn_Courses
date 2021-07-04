5002799de1b2d185b46759180c1bff3173e8a341
ClemXiX <clemsbo51@gmail.com>
2021-07-04 15:22:32 +0200

    Hello GitHub

---
+++ C#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cours_SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 22;
+
+            Console.WriteLine("nombre numéro 1 = {0}; y={1}", x, y);
+        }
+    }
+}
---
+++ Python/Beginner/test.py
@@ -0,0 +1,10 @@
+# 3 " inside a print() alow to outup on newline whiout the need
+# to write a \n
+
+print("""alors
+on
+danse""")
+
+print("alors"
+      "on"
+      "danse")
